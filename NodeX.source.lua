406a765cf6579f78714544d6e96f679fe3a66150
luarmor.net <ezzattymarwan@gmail.com>
2025-09-20 12:13:50 +0100

    Create NodeX.source.lua

---
+++ NodeX.source.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NodeX-Enc/NodeX/refs/heads/main/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/M4RUXX/Blade-ball/refs/heads/main/blade%20ball.lua"))()
