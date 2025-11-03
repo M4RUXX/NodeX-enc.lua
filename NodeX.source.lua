b92be83ae0d4904e6be3a0933b41bd45cde6a89b
luarmor.net <ezzattymarwan@gmail.com>
2025-11-03 15:04:43 +0100

    Update NodeX.source.lua

--- NodeX.source.lua
+++ NodeX.source.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NodeX-Enc/NodeX/refs/heads/main/Main.lua"))()
 loadstring(game:HttpGet("https://paste.debian.net/plainh/3b4667fa/", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NodeX-Enc/NodeX/refs/heads/main/Main.lua"))()
