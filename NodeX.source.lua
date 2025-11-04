f771642f37e2a0f86ec194c0d97caee337d529f6
luarmor.net <ezzattymarwan@gmail.com>
2025-11-04 13:38:48 +0100

    Update script URL to new source

--- NodeX.source.lua
+++ NodeX.source.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://paste.debian.net/plainh/3b4667fa/", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Akash1al/Blade-Ball-Updated-Script/refs/heads/main/Blade-Ball-Script"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/c458c939651a0abd1aa5898726665597ab7ed51952b694f518f43333f85628ec/download"))()
