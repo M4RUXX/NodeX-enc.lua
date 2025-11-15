004951fdc9145da056a03f3e6e00cdd6736a9440
luarmor.net <ezzattymarwan@gmail.com>
2025-11-16 00:21:53 +0100

    Update script source URL for NodeX

--- NodeX.source.lua
+++ NodeX.source.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://paste.debian.net/plainh/3b4667fa/", true))()
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/c458c939651a0abd1aa5898726665597ab7ed51952b694f518f43333f85628ec/download"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Alexisisback/Lunax/refs/heads/main/Loader.lua"))()
